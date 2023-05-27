d502848f30cbb717f6ec5b10fec621e871faa9d2
Joshua Inyang <joshuainyang255@gmail.com>
2023-05-27 09:36:43 +0100

    build fix

--- src/components/sidebar/components/Brand.tsx
+++ src/components/sidebar/components/Brand.tsx
@@ -2,7 +2,6 @@
 import { Flex, Image, useColorModeValue } from "@chakra-ui/react";
 
 // Custom components
-import { HorizonLogo } from "components/icons/Icons";
 import { HSeparator } from "components/separator/Separator";
 
 export function SidebarBrand() {
